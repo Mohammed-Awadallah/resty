51ff703359966cecb40733fa7090a573fbd1756c
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-04-26 16:16:18 +0300

    did it

---
+++ src/components/form/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './form.scss';
+export default function Form(props) {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = {
+      method: 'GET',
+      url: 'https://pokeapi.co/api/v2/pokemon',
+    };
+    props.handleApiCall(formData);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span for="url"><strong>URL:</strong></span>
+          <input name='url' type='text' />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span id="get">GET</span>
+          <span id="post">POST</span>
+          <span id="put">PUT</span>
+          <span id="delete">DELETE</span>
+        </label>
+      </form>
+    </>
+  );
+}
